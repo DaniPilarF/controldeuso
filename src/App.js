406ff17eb01c3b29fe0be3f38344fd62aa7a366a
Daniela Fernandez <dfernandez@unisoftnt.com.ar>
2018-10-31 17:58:10 -0300

    Maquetado de detalle de cliente

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Header from './components/Header';
 import Card from './components/Card';
 import HomeSearchBar from './components/HomeSearchBar';
 
+import {Route, Switch} from 'react-router-dom';
+import {getScoreColor, getStarsCategory} from "./utils/Category";
+
 import './App.scss';
 import './index.css';
 
@@ -131,15 +134,55 @@ class App extends Component {
         const usuario = this.state.usuario;
         const clientsList = this.state.clientsList;
 
+        const scoreColor = getScoreColor(6);
         return (
             <div className={theme}>
                 <Header userName={usuario.nombre}
                         clientsList={clientsList}
                         onThemeSelected={this.changeTheme}
                 />
-                <HomeSearchBar/>
+                {/* <HomeSearchBar/>
                 <div className='cards-container layout-row layout-wrap'>
                     {this.renderCards()}
+                </div> */}
+                <div className='client-container layout-column' >
+                    <div className='title-container layout-row'>
+                        <div>
+                            <div className='subtitle'>On Cloud</div>
+                            <div className='title'>Cliente 1</div>
+                            <div className='category layout-row'>
+                                <div className='stars'>
+                                    stars
+                                </div>
+                                <div className='subtitle'>CLIENTE AA</div>
+                            </div>
+                        </div>
+                        <div className='flex'></div>
+                        <div className='stats-container layout-row'>
+                            <div>
+                                <div className='subtitle-dark'>VERSIÓN</div>
+                                <div className='version'>189.25.23</div>
+                                <div className='date'>12 de Marzo 2018</div>
+                            </div>
+                            <div className='score' style={{ backgroundColor: scoreColor }}>
+                                6
+                            </div>
+                        </div>
+                    </div>
+                    <div className='bigCard-container'>
+                        <div className='big-card'>
+                            <div className='bigcard-title-container layout-row'>
+                                <div className='title'>Uso</div>
+                                <div className='flex'></div>
+                                <div className='texto-base'>Comparar con:</div>
+                                <div className='text-link active'>período anterior</div>
+                                <div className='text-link'>Objetivo</div>
+                            </div>
+                            <div className='bigcard-content-container layout-row'>
+                              <div flex></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
